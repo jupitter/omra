1608cfacca5f8212b2691242e90b220b4a505ba4
jupitter <rds_wang@hotmail.com>
2021-02-10 10:48:32 +0100

    Add test case for index page
    Gets the url and checks if http status code is 200,
    and checks if the page contains the text "Ordina Music Rater"

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index(self):
+        url = reverse('index')
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, "Ordina Music Rater")
