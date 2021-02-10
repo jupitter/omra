79109887e060fea18553893a8aceebeb79da738c
jupitter <rds_wang@hotmail.com>
2021-02-10 10:50:49 +0100

    Created index homepage and added the correct url path's
    Index homepage displays the context which is the title and some text.

--- song_rater/views.py
+++ song_rater/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
+def index(request):
+    context = {"title" : "Welcome To The Ordina Music Rater",
+               "text": "Make yourself at home"
+
+    }
+
+    return render(request, 'song_rater/index.html', context)
+
+
+
