372550690200adc970ca16c62af182fe21eaf2d0
jupitter <rds_wang@hotmail.com>
2021-02-16 15:58:17 +0100

    Added testcase for Song_list page.
    It checks if you can reach the page, did not yet
    add the ability to check for songs on the page.
    Will discuss this on 2/17/2021 at the stand-up.

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
+from .models import Song
 
 # Create your tests here.
 class IndexViewTest(TestCase):
@@ -9,3 +10,12 @@ class IndexViewTest(TestCase):
         r = self.client.get(url)
         self.assertEqual(r.status_code, 200)
         self.assertContains(r, "Ordina Music Rater")
+
+class SongListTest(TestCase):
+    def test_no_songs(self):
+        """If no songs exist, an appropriate message is displayed"""
+        url = reverse('song_rater:song_list')
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+
+
