2a2475490f4b1a2d2fc16d90c2d3e1acdff8fdae
jupitter <rds_wang@hotmail.com>
2021-02-16 23:36:07 +0100

    added song_detail page for views
    added the view for song_detail page.

--- song_rater/views.py
+++ song_rater/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from song_rater.models import Song
 from django.views.generic.list import ListView
 
@@ -15,3 +15,10 @@ def index(request):
 class SongListView(ListView):
     model = Song
     context_object_name = 'song_list'
+
+def song_detail(request, id):
+    context = {"details": get_object_or_404(Song, pk=id),
+    }
+
+    return render(request, 'song_rater/song_detail.html', context)
+
