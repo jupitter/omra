10700057302f3273f54f8452a7bac2f66b3af636
jupitter <rds_wang@hotmail.com>
2021-02-19 09:15:23 +0100

    Added Rating class and Rating Choices
    Ratings vary from 1-5 / trash to excellent.
    Migrated the class to the database

--- song_rater/models/song.py
+++ song_rater/models/song.py
@@ -7,3 +7,17 @@ class Song(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.artist}"
+
+
+RATING_CHOICES = [
+    (1, "1 - Trash"),
+    (2, "2 - Bad"),
+    (3, "3 - Average"),
+    (4, "4 - Great"),
+    (5, "5 - Excellent"),
+]
+
+
+class Rating(models.Model):
+    rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
