e2b29c60493b6755c10968e1399d4429d257f7bf
jupitter <rds_wang@hotmail.com>
2021-02-23 11:52:23 +0100

    Added Urls
    Added url for the add song page,
    also added a url link on all other pages to
    redirect back to the home page.

--- song_rater/urls.py
+++ song_rater/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('song_list', views.SongListView.as_view(), name='song_list'),
     path('song_details/<int:id>', views.song_detail, name='song_detail'),
+    path('add_song', views.add_song, name='add_song')
 ]
