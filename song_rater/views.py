212bba74764d15b134d703d73fa8f810fb7fd620
jupitter <rds_wang@hotmail.com>
2021-02-23 11:51:51 +0100

    Added add_song function
    Added the forms needed to send and save
    the title and artist and insert the data into the
    Song model in the database.

--- song_rater/views.py
+++ song_rater/views.py
@@ -40,3 +40,15 @@ def song_detail(request, id):
                }
 
     return render(request, 'song_rater/song_detail.html', context)
+
+
+def add_song(request):
+    if request.method == "POST":
+        if request.POST.get('title') and request.POST.get('artist'):
+            add_song = Song()
+            add_song.title = request.POST.get('title')
+            add_song.artist = request.POST.get('artist')
+            add_song.save()
+            return render(request, 'song_rater/add_song.html')
+    else:
+        return render(request, 'song_rater/add_song.html')
